9cc4e1758ef08a1f94c9990c473e64b235b29342
claire-labry <clairestjlabry@gmail.com>
2020-03-14 09:22:18 -0500

    adding completed HTML an CSS and beginning of Javascript

---
+++ Assets/javascript/app.js
@@ -0,0 +1,18 @@
+
+// Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyA4q8acgkEvZF1623DXF4VaNdnJpWUWURo",
+        authDomain: "train-scheduler-dfff0.firebaseapp.com",
+        databaseURL: "https://train-scheduler-dfff0.firebaseio.com",
+        projectId: "train-scheduler-dfff0",
+        storageBucket: "train-scheduler-dfff0.appspot.com",
+        messagingSenderId: "946340879207",
+        appId: "1:946340879207:web:ccc36590757b027c33adc2"
+    };
+  
+// Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+// Variable that references to Firebase
+
+    var database = firebase.database();
