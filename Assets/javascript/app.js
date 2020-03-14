462b968d9a84ed447a70868e734ebb27b8519375
claire-labry <clairestjlabry@gmail.com>
2020-03-14 18:14:41 -0500

    completed the bodies of each file

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,5 +1,4 @@
-
-// Your web app's Firebase configuration
+// Initialize Firebase
     var firebaseConfig = {
         apiKey: "AIzaSyA4q8acgkEvZF1623DXF4VaNdnJpWUWURo",
         authDomain: "train-scheduler-dfff0.firebaseapp.com",
@@ -10,9 +9,92 @@
         appId: "1:946340879207:web:ccc36590757b027c33adc2"
     };
   
-// Initialize Firebase
     firebase.initializeApp(firebaseConfig);
 
-// Variable that references to Firebase
+// Variable that links to Firebase
 
     var database = firebase.database();
+
+// On click function to add trains 
+
+   $("#add-train-btn").on("click", function(event){
+       event.preventDefault();
+
+       var trainName = $("#train-name-input").val().trim();
+       var trainDest = $("#destination-input").val().trim();
+       var firstTrainTime = $("#first-train-input").val().trim();
+       var trainFreq =  $("#frequency-input").val().trim();
+
+       var newTrain ={
+
+        name: trainName,
+        destination: trainDest,
+        firstTime: firstTrainTime,
+        frequency: trainFreq,
+
+       };
+
+       database.ref().push(newTrain);
+
+// Console logs to check the values that were pushed into Firebase
+
+       console.log(newTrain.name)
+       console.log(newTrain.destination)
+       console.log(newTrain.firstTime)
+       console.log(newTrain.frequency)
+
+       alert("New Train Added!")
+
+       $("#train-name-input").val("")
+       $("#destination-input").val("")
+       $("#first-train-input").val("")
+       $("#frequency-input").val("")
+
+   });
+
+   database.ref().on("child_added", function(childSnapshot){
+       console.log(childSnapshot.val());
+    
+       var trainName = childSnapshot.val().name;
+       var trainDest = childSnapshot.val().destination;
+       var firstTrainTime = childSnapshot.val().firstTime;
+       var trainFreq =  childSnapshot.val().frequency;
+
+       console.log(trainName);
+       console.log(trainDest);
+       console.log(firstTrainTime);
+       console.log(trainFreq);
+
+
+    // calculating next trains arriving 
+    
+    var firstTrainTimeCurr = moment(firstTrainTime, "hh:mm a").subtract(1, "years")
+   
+    var currentTime = moment().format("hh:mm a");
+    console.log(currentTime)
+    
+    var trainTimeDiff = moment().diff(moment(firstTrainTimeCurr), "minutes");
+    
+    var timeLeft = trainTimeDiff % trainFreq;
+    
+    var minAway = trainFreq - timeLeft;
+    
+    var nextArr = moment().add(minAway, "minutes").format("hh:mm a")
+    console.log(nextArr);
+
+
+    // creating a new train row
+
+    var newTrainRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(trainDest),
+        $("<td>").text(trainFreq),
+        $("<td>").text(nextArr),
+        $("<td>").text(minAway),
+      );
+
+    // Append the new train row
+    
+    $("#table > tbody").append(newTrainRow);
+
+   }); // end syntax
